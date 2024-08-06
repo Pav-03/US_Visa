d22b697a4af06934bf984601ef3c13f33aab300f
pavanmodi <pmodi1203@gmail.com>
2024-08-06 11:18:47 +0100

    Install requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Pavan",
+    author_email="pmodi.ml.1203@gmail.com",
+    packages=find_packages()
+)
